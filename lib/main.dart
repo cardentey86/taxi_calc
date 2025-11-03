070595a7d79dd049cb74a031f1d8037cc1a95d15
adrianalvarez <adrianalvarezcardentey@gmail.com>
2025-11-03 16:37:57 -0500

    update manifest

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
 import 'package:taxi_calc/modules/map/screens/map_screen.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MyApp());
