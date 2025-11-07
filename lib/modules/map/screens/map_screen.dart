b9eca9a384a1c4fcc7ea4307b4c3613eaa435c65
adrianalvarez <adrianalvarezcardentey@gmail.com>
2025-11-07 11:21:17 -0500

    fix warnings

--- lib/modules/map/screens/map_screen.dart
+++ lib/modules/map/screens/map_screen.dart
@@ -1,8 +1,5 @@
 import 'dart:async';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
